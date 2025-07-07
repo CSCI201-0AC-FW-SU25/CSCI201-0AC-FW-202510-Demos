04f6384c7eb6e1177c6480d5ca1cbad46cb3fa80
Angela Venable <avenable11@ivytech.edu>
2025-07-07 16:12:19 -0400

    lecture activity

--- 07_07_25/main.cpp
+++ 07_07_25/main.cpp
@@ -2,33 +2,61 @@
 #include <limits>
 #include <string>
 #include <random>
+#include <vector>
 #include "clock.h"
 
 void resetStream();
 template <class Type>
 Type inputNum(std::string prompt, std::string err, Type high, Type low);
 // double inputNum(std::string prompt, std::string err, double high, double low);
+// lecture activity create size maps for the drink class.
 
 int main()
 {
-    double x;
-    x = inputNum<double>("Enter a number", "That is not what I wanted~!", 100.5, 0.25);
+    // double x;
+    // x = inputNum<double>("Enter a number", "That is not what I wanted~!", 100.5, 0.25);
+    std::vector<twentyFourHrClock> clocks;
+    std::vector<clockType *> clocksAgain;
+
     std::default_random_engine generator;
     std::uniform_int_distribution<int> hrDist(0, 23);
     std::uniform_int_distribution<int> minSecDist(0, 59);
+    std::uniform_int_distribution<int> boolDist(0, 1);
+    std::uniform_int_distribution<int> twelveHrDist(1, 12);
     twentyFourHrClock clock;
-    twentyFourHrClock clock1;
-    clock1.setTime(hrDist(generator), minSecDist(generator), minSecDist(generator));
-
-    twentyFourHrClock clock2;
-    clock2.setTime(hrDist(generator), minSecDist(generator), minSecDist(generator));
-    if (clock1 < clock2)
+    for (int i = 0; i < 2; i++)
     {
-        clock = inputNum<twentyFourHrClock>("Enter the hour minutes seconds for the clock.", "Soomething went wrong try again.", clock2, clock1);
+        clocks.push_back(twentyFourHrClock(hrDist(generator), minSecDist(generator), minSecDist(generator)));
+    }
+    int moreClocks = 1;
+    while (moreClocks == 1)
+    {
+        if (boolDist(generator))
+        {
+            clocksAgain.push_back(new twentyFourHrClock(hrDist(generator), minSecDist(generator), minSecDist(generator)));
+        }
+        else
+        {
+            clocksAgain.push_back(new twelveHrClock(twelveHrDist(generator), minSecDist(generator), minSecDist(generator), boolDist(generator) == 0 ? partType::AM : partType::PM)); //?: does if-else on boolDist(generator)
+        }
+        moreClocks = boolDist(generator);
+    }
+    // clock1.setTime(hrDist(generator), minSecDist(generator), minSecDist(generator));
+
+    // clock2.setTime(hrDist(generator), minSecDist(generator), minSecDist(generator));
+    if (clocks[0] < clocks[1])
+    {
+        clock = inputNum<twentyFourHrClock>("Enter the hour minutes seconds for the clock.", "Soomething went wrong try again.", clocks[1], clocks[0]);
     }
     else
     {
-        clock = inputNum<twentyFourHrClock>("Enter the hour minutes seconds for the clock.", "Soomething went wrong try again.", clock1, clock2);
+        clock = inputNum<twentyFourHrClock>("Enter the hour minutes seconds for the clock.", "Soomething went wrong try again.", clocks[0], clocks[1]);
+    }
+
+    for (int i = 0; i < clocksAgain.size(); i++)
+    {
+        std::cout << *clocksAgain[i] << std::endl;
+        delete clocksAgain[i];
     }
 
     return 0;
