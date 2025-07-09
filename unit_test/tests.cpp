600afda07043c7b742d686763775ea29aa58e7fc
Angela <avenable11@ivytech.edu>
2025-07-09 16:46:54 -0400

    excpetions in unit tests.

--- unit_test/tests.cpp
+++ unit_test/tests.cpp
@@ -2,19 +2,22 @@
 #include <gmock/gmock.h>
 #include <regex>
 #include <cmath>
-#include "../07_02_25/clock.h"
+#include "../07_09_25/clock.h"
+
 using namespace std;
 
 TEST(twentyFourHrClockTest, equality)
 {
-	twentyFourHrClock c1(13, 0, 0);
+	EXPECT_NO_THROW(twentyFourHrClock c1(13, 0, 0););
+	EXPECT_ANY_THROW(twelveHrClock c5(21, 0, 0, partType::PM));
+	EXPECT_THROW(twentyFourHrClock c4(44, 0, 0), invalid_hour);
 	twelveHrClock c2(1, 0, 0, partType::PM);
 	twentyFourHrClock c3(13, 3, 3);
-	EXPECT_TRUE(c1 == c2);
-	EXPECT_FALSE(c1 == c3);
-	// your test code goes here.
-	// see https://google.github.io/googletest/ for more information on writing a unit test
-	// assertions reference https://google.github.io/googletest/reference/assertions.html
+	// EXPECT_TRUE(c1 == c2);
+	// EXPECT_FALSE(c1 == c3);
+	//  your test code goes here.
+	//  see https://google.github.io/googletest/ for more information on writing a unit test
+	//  assertions reference https://google.github.io/googletest/reference/assertions.html
 }
 
 // do not change any code below this comment
