220c1e3aeab882d9bb0189ab6a002be0dcb85594
avenable11 <avenable11@ivytech.edu>
2025-07-28 16:03:49 -0400

    first event handlers

--- GUI_Demos/DrinksV2/drinksWindow.cpp
+++ GUI_Demos/DrinksV2/drinksWindow.cpp
@@ -29,4 +29,39 @@ DrinksWindow::DrinksWindow()
 			checkCount++;
 		}
 	}
+	
+	scroller_view.base << [&, this]
+	{
+		
+		baseType b = bases[(int)scroller_view.base];
+		d.setBase(b);
+		checkPrice();
+		
+	};
+	scroller_view.temp << [&, this]
+	{
+		tempType t = temps[(int)scroller_view.temp];
+		d.setTemperature(t);
+		checkPrice();
+	};
+	scroller_view.size << [&, this]
+	{
+		sizeType s = sizes[scroller_view.size.GetData()];
+		d.setSize(s);
+		checkPrice();
+	};
+	
+	
+}
+
+void DrinksWindow::checkPrice()
+{
+	if((int)scroller_view.base != -1 && (int)scroller_view.temp != -1 &&
+	(int)scroller_view.size != -1)
+	{
+		std::ostringstream priceStr;
+		priceStr << std::setprecision(2) << std::fixed << std::showpoint;
+		priceStr << "$" << d.getPrice();
+		scroller_view.price.SetData(priceStr.str());
+	}
 }
--- GUI_Demos/DrinksV2/drinksWindow.h
+++ GUI_Demos/DrinksV2/drinksWindow.h
@@ -6,6 +6,10 @@
 #include "drinks.h"
 #include <AutoScroller/AutoScroller.h>
 #include <CtrlCore/lay.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
 
 using namespace Upp;
 
@@ -13,11 +17,16 @@ class DrinksWindow : public WithMainAppWindow<TopWindow>
 {
 public:
 	DrinksWindow();
+	void checkPrice();
 
 private:
 	AutoScroller scroller;
 	WithDrinksWindowLayout<ParentCtrl> scroller_view;
 	Option flavor[NUM_FLAV];
+	drink d;
+	std::vector<drink> order;
+	Button writeBtn;
+	OptionTree optree;
 };
 
 #endif
